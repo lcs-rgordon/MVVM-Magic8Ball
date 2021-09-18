345703c1f371867ff6b9f1e2ed8c3c8962004c93
Russell Gordon <rgordon@lcs.on.ca>
2021-09-17 20:56:19 -0400

    Prompt repeats and history of commands is now shown.

--- MVVM-Magic8Ball/Views/main.swift
+++ MVVM-Magic8Ball/Views/main.swift
@@ -14,21 +14,58 @@ var advisor = AdviceViewModel()
 print("MAGIC 8 BALL")
 print("============")
 
-// Ask for user's question
-print("""
-NOTE: Questions should be phrased such
-      that they can be answered with a
-      yes or no response.
+// Loop until user chooses to quit
+while true {
+    
+    // Ask for user's question
+    print("""
+    NOTE: Questions should be phrased such
+          that they can be answered with a
+          yes or no response.
 
-""")
-print("What is your question? ")
-// Get input as a non-optional String
-let input = readLine()!
+    """)
+    print("What is your question? ")
+    
+    // Get input as a non-optional String
+    let input = readLine()!
+    print("")
 
-// Print out their question
-print("")
-print("You said: \(input)")
-print("")
-
-// Provide the advice by making an instance of the Magic8Ball type and then getting a response
-print(advisor.provideResponseFor(givenQuery: input))
+    // Provide the advice by making an instance of the Magic8Ball type and then getting a response
+    print(advisor.provideResponseFor(givenQuery: input))
+    
+    // Ask whether the user wants to continue or see history of advice
+    while true {
+        print("")
+        print("See history of advice given? (Y/N)")
+        let selection = readLine()!
+        if selection == "Y" {
+            
+            // Show history
+            print("")
+            print("History")
+            print("-------")
+            for session in advisor.sessions {
+                print(session.question)
+                print(session.response)
+                print("")
+            }
+            break
+            
+        } else if selection == "N" {
+            break
+        }
+    }
+    
+    // Ask whether the user wants to see more advice
+    while true {
+        print("More advice? (Y/N)")
+        let selection = readLine()!
+        if selection == "Y" {
+            break
+        } else if selection == "N" {
+            exit(0)
+        }
+    }
+    
+    
+}
