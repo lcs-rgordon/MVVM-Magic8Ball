1619eaf0f959ed301ed2450e1c4cd9e20ac55a93
Russell Gordon <rgordon@lcs.on.ca>
2021-09-27 13:44:31 -0400

    watchOS target added and program runs on this platform.

---
+++ watchOS WatchKit Extension/MVVM_Magic8BallApp.swift
@@ -0,0 +1,19 @@
+//
+//  MVVM_Magic8BallApp.swift
+//  watchOS WatchKit Extension
+//
+//  Created by Russell Gordon on 2021-09-19.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_Magic8BallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
